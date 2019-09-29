a674c86e31c92a0ebdc281eea11554ca84d7ff3d
Henri Tammo <43129477+HenriTammo@users.noreply.github.com>
2019-09-30 00:03:15 +0300

    Probleem ül 5-s

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {laptops, phones} from "./mydatabase.js";
+//import {laptops, phones} from "./mydatabase.js";
 import Header from "./Header.jsx";
 import ItemList from "./ItemList.jsx";
 
@@ -8,7 +8,8 @@ class HomePage extends React.PureComponent{
   constructor(props){
     super(props);
     this.state = {
-      items: phones,
+      items: [],
+      selectedCategory: null,
     };
   }
 
@@ -24,36 +25,28 @@ class HomePage extends React.PureComponent{
     })
     .then(items => {
       console.log("items", items);
+      this.setState({
+        items
+      });
     })
     .catch(err => {
       console.log("err", err)
     });
   }
 
-  handleChange(event){
+  handleDropdown(event){
     console.log(event.target.value);
-    switch(event.target.value){
-      case "phones":{
-        this.setState({
-          items: phones,
-        });
-        break;
-      }
-      case "laptops":{
-        this.setState({
-          items: laptops,
-        });
-        break;
-      }
-    }
-    console.log("App state", this.state);
+    this.setState({
+      selectedCategory: event.target.value
+    });
+
   }
 
   render(){
     return (
       <>
         <Header />
-        <select onChange={this.handleChange.bind(this)}>
+        <select onChange={this.handleDropdown.bind(this)}>
           <option value="phones">Phones</option>
           <option value="laptops">Laptops</option>
         </select>
