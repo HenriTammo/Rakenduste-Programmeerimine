6997bd533f8250e81a01f136a99c3edba6063fc3
Henri Tammo <43129477+HenriTammo@users.noreply.github.com>
2019-11-09 16:13:49 +0200

    kiire fix

--- server/server.js
+++ server/server.js
@@ -1,28 +1,89 @@
-/*jshint esversion: 6 */
-const express = require('express')
-const app = express()
-const path= require("path");
+const express = require('express');
+const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-const DB = require("./database.js")
+const mongoose = require("mongoose");
+const itemRouter = require("./item.router.js");
+const authRouter = require("./auth.router.js");
+const userRouter = require("./user.router.js");
+const DB = require("./database.js");
+const Item = require("./item.model.js");
+const bodyParser= require("body-parser");
 
-app.get("/api/items", (req, res)=>{
-  res.json(DB.getItems());
-});
+/** Development environment. In Heroku we don't use .env file */
+if(process.env.NODE_ENV !== "production"){
+  require('dotenv').config();
+}
 
-app.get("/api/items/:itemId", (req, res)=>{
-  res.send(DB.getItem(req.params.itemId));
-});
+
+const DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0-p4gbz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
+app.use(bodyParser.json());
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1", itemRouter);
+app.use("/api/v1/users", userRouter);
 
 
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+});
+
+app.get('/items/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
-})
+});
 
 app.use(express.static('dist'));
 
-app.listen(PORT, () => {
-  console.log("Server started", PORT);
-  console.log(`http://localhost:${PORT}`);
-});
+function listen() {
+  app.listen(PORT, () => {
+    console.log("Server started", PORT);
+    console.log('http://localhost:${PORT}');
+  });
+}
 
-/*test*/
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log("Database access success!");
+    listen();
+    migrate();
+    //deleteAllItems();
+  })
+  .catch( err => {
+    console.log("error happened", err);
+  });
+
+  function migrate(){
+      Item.count({},(err, countNr) => {
+          if(err) throw err;
+          if(countNr > 0 ){
+            console.log("Already had items, don't save");
+            return;
+          }
+          saveAllItems();
+      });
+  }
+
+  function deleteAllItems(){
+      Item.deleteMany({}, (err, doc) => {
+        console.log('err', err, "doc", doc);
+      });
+  }
+
+  function saveAllItems(){
+    console.log("migrate started!");
+    const items = DB.getItems();
+    items.forEach(item=> {
+        const document = new Item(item);
+        document.save( (err) => {
+          if(err){
+              console.log(err);
+              throw new Error ("Something happened during save");
+          }
+          console.log('save succes');
+        });
+    });
+    console.log("items", items);
+  }
